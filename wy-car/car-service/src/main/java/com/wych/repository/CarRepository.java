ddd5c259439d53ec5a57a582d5542f78fad813ff
yy_glb <yy_glb@qq.com>
2020-01-15 10:03:07 +0800

    车源发布、查找车源，注册完成

--- wy-car/car-service/src/main/java/com/wych/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wych.repository;
-
-import com.wych.pojo.Car;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-/*public interface CarRepository extends ElasticsearchRepository<Car,Long> {
-}*/

32948b8a328b57f45bcda6e0ed7ae2de3d06a09a
whllog <whl0506@163.com>
2020-01-16 15:16:24 +0800

    最终合并

--- wy-monitor/src/main/java/com/wych/MonitorAdminApplication.java
+++ wy-monitor/src/main/java/com/wych/MonitorAdminApplication.java
@@ -1,13 +1,11 @@
 package com.wych;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableEurekaClient

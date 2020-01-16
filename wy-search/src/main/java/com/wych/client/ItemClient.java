14ffd24f2fb753de0db6486e48ee93146a22f3e9
whllog <whl0506@163.com>
2020-01-16 14:12:10 +0800

    更新spring cloud版本

--- wy-search/src/main/java/com/wych/client/ItemClient.java
+++
@@ -1,9 +0,0 @@
-package com.wych.client;
-
-import com.wych.api.GoodsApi;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient("goods-server")
-public interface ItemClient extends GoodsApi {
-
-}
